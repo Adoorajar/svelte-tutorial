903ea607465844984b1247aa5d45cb0ac1775769
Dobbs <Brian.Dobbs@gdit.com>
2021-09-23 10:50:26 -0500

    initial commit

---
+++ src/ExpensesList.svelte
@@ -0,0 +1,30 @@
+<script>
+  import SectionTitle from './Title.svelte'
+  import Expense from './Expense.svelte'
+  import { fly } from 'svelte/transition'
+  import { flip } from 'svelte/animate'
+  export let expenses = []
+</script>
+
+<section>
+  <SectionTitle title="expense list" />
+  <ul>
+    {#each expenses as expense, index (expense.id)}
+      <div
+        in:fly={{ x: 200, delay: (index + 1) * 350 }}
+        out:fly={{ x: -200 }}
+        animate:flip
+      >
+        <Expense {...expense} />
+      </div>
+    {:else}
+      <h2>no expenses added to the list!</h2>
+    {/each}
+  </ul>
+</section>
+
+<style>
+  h2 {
+    text-transform: capitalize;
+  }
+</style>
